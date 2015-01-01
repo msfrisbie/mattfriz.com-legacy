0f6d8cd1fb49680bc60fd39c0436261ee140cdc2
Matt Frisbie <msfrisbie@gmail.com>
2015-01-01 06:46:30 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mattfrizApp
+ * @description
+ * # mattfrizApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mattfrizApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mattfrizApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mattfrizApp
+ */
+angular.module('mattfrizApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
